cb0a4a6215eae339fb538b6030f59d3a3a3b0d41
Pawel-dev5 <p.nowecki@gmail.com>
2021-08-01 14:35:17 +0200

    feat: added API fetch function by useEffect, added function getCard whith slice item from fetching cards and push item to drawnCards array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({
+    cards: [],
+    drawnCards: [],
+    randomCard: "",
+  });
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch(
+        "https://deckofcardsapi.com/api/deck/new/draw/?count=52"
+      );
+      response = await response.json();
+      setData({
+        cards: response.cards,
+        drawnCards: [],
+      });
+    }
+    fetchMyAPI();
+  }, []);
+
+  function getCard() {
+    setData((prevState) => ({
+      drawnCards: [
+        ...prevState.drawnCards,
+        prevState.cards[prevState.cards.length - 1],
+      ],
+      cards: [...prevState.cards.slice(0, -1)],
+      randomCard: "",
+    }));
+  }
+
+  if (data) {
+    return (
+      <div className="App">
+        <h2>Card Bid Game</h2>
+        <button onClick={getCard}>Start</button>
+        <br />
+        {data.drawnCards.length === 0 ? (
+          <>
+            <p>Brak Kart</p>
+          </>
+        ) : (
+          <>
+            {data.drawnCards.map((item) => {
+              return (
+                <>
+                  <img src={item.image} alt="card"></img>
+                  <p>{item.code}</p>
+                </>
+              );
+            })}
+          </>
+        )}
+      </div>
+    );
+  } else return <h1>Ładuję</h1>;
 }
 
 export default App;
